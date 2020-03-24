91409bcd99f1bc99bf0776980acef0a1f7c85d91
jineshshah101 <60665848+jineshshah101@users.noreply.github.com>
2020-03-24 13:20:45 -0400

    Add files via upload

---
+++ Numbers.js
@@ -0,0 +1,44 @@
+// Can have numbers in javascript
+
+var num1 = 20;
+var num2 = 15;
+var num3 = 10;
+var num4 = 5;
+var num5 = 20;
+var num6 = 30;
+var num7 = 40;
+
+// increment number by 1
+
+var incrnum = num1++
+window.alert(incrnum);
+
+// decrement number by 1
+
+var decrnum = num2--
+window.alert(decrnum);
+
+// divide numbers
+
+var divnum = num3/2;
+window.alert(divnum);
+
+// multiply numbers
+
+var mulnum = num4 * 5;
+window.alert(mulnum);
+
+// getting remainder values of numbers
+
+var remanum = num5 % 3;
+window.alert(remanum);
+
+// increment by any number you wish
+
+var incanynum = num6 += 10;
+window.alert(incanynum);
+
+// decrement by any number you wish
+
+var decanynum = num7 -= 10;
+window.alert(decanynum);
